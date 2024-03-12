8e977cb93e4f601481e95bde2598e47282f5cf1b
Anton Pavliuk <streetreadger@gmail.com>
2024-03-13 00:19:51 +0200

    Add api test and structure for tests
    Also adding xdist plugin for running in few workers
    
    using pytest -n 3

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,10 +1,12 @@
 import pytest
 
 
+@pytest.mark.math
 def test_one_plus_one():
     assert 1 + 1 == 2
 
 
+@pytest.mark.math
 def test_one_plus_two():
     a = 1
     b = 2
@@ -12,6 +14,7 @@ def test_one_plus_two():
     assert a + b == c
 
 
+@pytest.mark.math
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError) as e:
         num = 1 / 0
@@ -30,5 +33,6 @@ products = [
 
 
 @pytest.mark.parametrize('a, b, product', products)
+@pytest.mark.math
 def test_multiplication(a, b, product):
     assert a * b == product
